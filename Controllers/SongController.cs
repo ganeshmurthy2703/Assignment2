94405fa1f1fc8da0665d2405966061306b2cf105
ganeshmurthy2703 <ganeshmurthy2027@gmail.com>
2021-02-06 11:11:18 -0500

    Added new song controller

---
+++ Controllers/SongController.cs
@@ -0,0 +1,37 @@
+﻿using Assignment2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Controllers
+{
+    public class SongController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public SongController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Song1()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
